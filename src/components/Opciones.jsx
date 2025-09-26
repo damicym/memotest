1e7d172bb04602a11c6ac8e98aeb1fcde2f11174
Dami <damiancym05@gmail.com>
2025-09-25 22:42:12 -0300

    tooltip de estado

--- src/components/Opciones.jsx
+++ src/components/Opciones.jsx
@@ -1,5 +1,8 @@
 import { isPrimeOrBanned, getClosestNotPrimeOrBanned } from "../libs/myFunctions"
 import { GAME_RULES } from "./Juego"
+import { FaInfoCircle as InfoIcon } from "react-icons/fa";
+import Tooltip from 'react-bootstrap/Tooltip';
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
 
 function Opciones({ totalPairs, setTotalPairs, prevValuePairs }){
     const handleChange = e => {
@@ -19,7 +22,19 @@ function Opciones({ totalPairs, setTotalPairs, prevValuePairs }){
 
     return (
         <section className='opciones'>
-            <h1>Modo Casual</h1>
+            <div className="title">
+                <h1>Modo Casual</h1>
+                <OverlayTrigger
+                        delay={{ show: 200, hide: 0 }}
+                        key='infoTitleOverlay'
+                        placement='right'
+                        overlay={
+                            <Tooltip className="customTooltip" id='tooltip-right'>Esto es una beta. Próximamente habrá nuevos modos, desafíos diarios y leaderbaord de jugadores</Tooltip>
+                        }
+                    >
+                    <InfoIcon size={18}></InfoIcon>
+                </OverlayTrigger>
+            </div>
             <div className="campo">
                 <label>Pares de fichas ({totalPairs})</label>
                 <input className="controls" type='range' min="4" step="1" max="50" onChange={ (e) => handleChange(e) } value={totalPairs}></input>
